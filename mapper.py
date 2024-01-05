36d7c718ed6776f2af36761f8f124231b31c6a9a
aryagrao <ar1422@rit.edu>
2024-01-04 22:24:57 -0500

    Added code for a simple mapper using ray library.

---
+++ mapper.py
@@ -0,0 +1,43 @@
+import ray
+from consistent_hashing import ConsistentHashing
+
+
+@ray.remote
+class Mapper:
+    def __init__(self, mapper, name, coordinator_name, reducer_queues, autoscale=False, *args):
+        self.mapper = mapper
+        self.name = name
+        self.reducer_queues = reducer_queues
+        self.coordinator_name = coordinator_name
+        self.done = False
+        self.ch = ConsistentHashing(nodes=len(self.reducer_queues))
+        self.autoscale = autoscale
+
+        self.autoscaler = ray.get_actor("autoscaler")
+        self.autoscaler.register_mapper.remote(self.name)
+
+    def add_to_queue(self, data):
+        output = self.mapper[data]
+        index = ray.get(self.autoscaler.key_lookup.remote(output))
+        self.reducer_queues[index].put(output)
+
+    def process(self):
+        coordinator = ray.get_actor(self.coordinator_name)
+
+        while True:
+            data = ray.get(coordinator.mapper_input.remote())
+            if data is not None:
+                self.add_to_queue(data)
+            else:
+                break
+
+        for reducer_queue in self.reducer_queues:
+            reducer_queue.put(None)
+
+        self.done = True
+
+    def reschedule_output(self, node_idx):
+        self.ch.halve_tokens_for_node(node_idx=node_idx)
+
+    def done(self):
+        return self.done
